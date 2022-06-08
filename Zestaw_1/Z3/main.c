88eecd1a0ddb02710b6455da5fa24f7f983ba69b
Nikodem <97964965+Nikodem-P@users.noreply.github.com>
2022-06-08 11:49:17 +0200

    Poprawiono dwuwymiarowe tablice elementów na dwuwymiarowe tablice tablic

--- Zestaw_1/Z3/main.c
+++ Zestaw_1/Z3/main.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int howManyEven(int n, int m, int tab1[n][m], int tab2[n][m]);
+int counttdEven(int n, int m, int** tab1, int** tab2);
+
+int** arrtdCreate(int n, int m)
+{
+    int** tab = (int**)malloc(sizeof(int*) * n);
+    for(int i = 0; i < n ; i++)
+    {
+        *(tab + i) = (int*)malloc(sizeof(int) * m);
+    }
+    return tab;
+}
 
 int main() {
-    int tablica1[][3] = {{1,2,3}, {2,3,4}, {4,5,6}};
-    int tablica2[][3] = {{0,2,3}, {0,3,4}, {0,4,5}};
-    printf("Wynik: %d\n", howManyEven(3, 3, tablica1, tablica2));
+    int** tablica1 = arrtdCreate(2, 3);
+    int** tablica2 = arrtdCreate(2, 3);
+    *(*(tablica1+0)+0) = 0;
+    *(*(tablica1+0)+1) = 1;
+    *(*(tablica1+0)+2) = 2;
+    *(*(tablica1+1)+0) = 3;
+    *(*(tablica1+1)+1) = 4;
+    *(*(tablica1+1)+2) = 5;
+    *(*(tablica2+0)+0) = 6;
+    *(*(tablica2+0)+1) = -7;
+    *(*(tablica2+0)+2) = -8;
+    *(*(tablica2+1)+0) = -9;
+    *(*(tablica2+1)+1) = -10;
+    *(*(tablica2+1)+2) = -11;
+
+    printf("Parzyste: %d\n", counttdEven(2, 3, tablica1, tablica2));
     return 0;
 }
 
-int howManyEven(int n, int m, int tab1[n][m], int tab2[n][m])
+int counttdEven(int n, int m, int** tab1, int** tab2)
 {
     int result = 0;
     for(int i = 0; i < n; i++)
